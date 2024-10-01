31bebb37d07164395a64ea43bf95f5ca50e5ac05
f-sara <jyujyusa.68@gmail.com>
2024-10-01 23:04:04 +0900

    feat: decodeAPIResponseを作成

--- Sources/APIClient/APIClient.swift
+++ Sources/APIClient/APIClient.swift
@@ -1,4 +1,16 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+import Foundation
 
+class APIClient {
+
+
+    public func decodeAPIResponse<T: Decodable>(_ response: Data) throws -> T {
+        let decoder = JSONDecoder()
+        let data = try decoder.decode(T.self, from: response)
+        return data
+    }
+
+
+}
